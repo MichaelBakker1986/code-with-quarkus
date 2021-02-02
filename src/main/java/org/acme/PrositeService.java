27763148b66de84e5dd10e152a8e0c455300aa4a
Michael Bakker <michael.bakker1986@gmail.com>
2021-02-02 01:17:54 +0100

    First version - DB backup

--- src/main/java/org/acme/PrositeService.java
+++ src/main/java/org/acme/PrositeService.java
@@ -1,15 +1,13 @@
 package org.acme;
 
 import com.google.gson.Gson;
+import lombok.val;
 import nl.appmodel.Base64;
 import nl.appmodel.DataObjectPro;
 import nl.appmodel.HibernateUtilWithJavaConfig;
 import nl.appmodel.Pro;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -17,25 +15,27 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 @Path("/search")
 public class PrositeService {
-    Session session = HibernateUtilWithJavaConfig
-            .getSessionFactory()
-            .openSession();
-    Gson    gson    = new Gson();
+    Gson gson = new Gson();
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public String hello() {
-        CriteriaBuilder    cb   = session.getCriteriaBuilder();
-        CriteriaQuery<Pro> cr   = cb.createQuery(Pro.class);
-        Root<Pro>          root = cr.from(Pro.class);
+        Session session = HibernateUtilWithJavaConfig
+                .getSessionFactory()
+                .openSession();
+        val cb   = session.getCriteriaBuilder();
+        val cr   = cb.createQuery(Pro.class);
+        val root = cr.from(Pro.class);
         cr.select(root);
         cr.orderBy(cb.desc(root.get("views")));
+        cr.where(cb.equal(root.get("downloaded"), true));
 
         Query<Pro> query = session.createQuery(cr);
         query.setMaxResults(100);
         query.setHint("org.hibernate.cacheable", true);
         query.setCacheable(true);
-        List<Pro> list   = query.list();
-        var       object = list.stream().map(l -> new DataObjectPro(l.getId(), 1, Base64.fromId(l.getId()))).toArray();
+        List<Pro> list = query.list();
+        session.close();
+        var object = list.stream().map(l -> new DataObjectPro(l.getId(), 1, Base64.fromId(l.getId()))).toArray();
         /*var o = session.createQuery("FROM Pro order by views DESC")
                        .setMaxResults(100)
                        .setHint("org.hibernate.cacheable", true).getResultList();
