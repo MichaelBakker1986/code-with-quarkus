f1fcb2c7f7c396deba89e696c241d0e78f9e0fff
MichaelBakker1986 <michael.bakker1986@gmail.com>
2021-02-12 15:38:39 +0100

    add hamburger

--- src/main/java/nl/appmodel/service/LookupService.java
+++ src/main/java/nl/appmodel/service/LookupService.java
@@ -38,9 +38,9 @@ public class LookupService {
     public String hello() {
         Object[] x = util.session("lookup/" + name, s -> {
             var sql = "SELECT pro_id as id,downloaded,p.views as views,thumbs,header,embed from promyis p " +
-                      "join pro_tags pt on pt.pro=p.pro_id " +
-                      "join tags t on t.id=pt.tag " +
                       "join pro pp on pp.id =p.pro_id " +
+                      "join pro_tags pt on pt.pro=pp.id " +
+                      "join tags t on t.id=pt.tag " +
                       "where pp.downloaded=1 " +
                       "and t.name=:name";
             Query<Pro> query = s.createNativeQuery(sql, Pro.class);
