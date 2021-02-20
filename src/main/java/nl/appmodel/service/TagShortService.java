d6e4c9a63018008b2c73d04caf5cb09b0b964e48
MichaelBakker1986 <michael.bakker1986@gmail.com>
2021-02-20 04:08:11 +0100

    Resize images, full navigation click

--- src/main/java/nl/appmodel/service/TagShortService.java
+++ src/main/java/nl/appmodel/service/TagShortService.java
@@ -1,9 +1,9 @@
 package nl.appmodel.service;
 
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import nl.appmodel.Tags;
 import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
 import org.jboss.resteasy.annotations.cache.Cache;
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -20,18 +20,31 @@ public class TagShortService {
     @Produces(MediaType.APPLICATION_JSON)
     @Cache(maxAge = 43200)
     public Response tags() {
-        if (short_name.length() < 2) return Response.ok(new ArrayList<>()).build();
-        val q = s.createNativeQuery(
-                "select id,LOWER(name) as name,mt.popularity as popularity From most_popular mt inner join tags t on mt.tag_id = t.id  where short_2=:short_name and popularity > 1 order by mt.popularity desc",
-                Tags.class);
-        q.setParameter("short_name", shorten(short_name));
-        q.setMaxResults(500);
+        if (short_name.length() < 2) return empty();
+        NativeQuery<Tags> q;
+        if (short_name.length() == 2) {
+            q = s.createNativeQuery(
+                    "select id,LOWER(name) as name,mt.popularity as popularity From most_popular mt inner join tags t on mt.tag_id = t.id  where short_2=:short_name and popularity > 1 order by mt.popularity desc",
+                    Tags.class);
+            q.setParameter("short_name", short_name);
+        } else if (short_name.length() >= 3) {
+            q = s.createNativeQuery(
+                    "select id,LOWER(name) as name,mt.popularity as popularity From most_popular mt inner join tags t on mt.tag_id = t.id  where short_3=:short_name and popularity > 1 order by mt.popularity desc",
+                    Tags.class);
+            q.setParameter("short_name", shorten());
+        } else {
+            return empty();
+        }
+        q.setMaxResults(20);
         q.setReadOnly(true);
         q.setHint("org.hibernate.cacheable", true);
         q.setCacheable(true);
         return Response.ok(q.list()).build();
     }
-    private String shorten(String short_name) {
-        return short_name.substring(0, 2);
+    private Response empty() {
+        return Response.ok(new ArrayList<>()).build();
+    }
+    private String shorten() {
+        return short_name.substring(0, 3);
     }
 }
