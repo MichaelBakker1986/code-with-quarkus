b2037d27110370b175007db3548cbf3cce256307
MichaelBakker1986 <michael.bakker1986@gmail.com>
2021-02-26 19:26:41 +0100

    update port for initialState

--- src/main/java/nl/appmodel/service/OpenCVService.java
+++ src/main/java/nl/appmodel/service/OpenCVService.java
@@ -9,18 +9,18 @@ import lombok.extern.slf4j.Slf4j;
 @Getter
 @AllArgsConstructor
 public class OpenCVService {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
      /*   System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         var matMat = new Mat<Mat>();
         var src    = Imgcodecs.imread("\\\\SERVER\\flat\\DUFMP", Imgcodecs.IMREAD_GRAYSCALE);
         System.out.println(String.valueOf(src.rows()));*/
-  }
-  public void test() {
+    }
+    public void test() {
 
    /*   = cv2.imencode(".webp",
                                    img,
                 [cv2.IMWRITE_WEBP_QUALITY, 100])
 
         img = cv2.imdecode(buf, 1) */
-  }
+    }
 }
